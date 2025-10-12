a458e91cc1523cdbdfb38650da51a527d43ae7c9
deniseleon <kbenisee7@gmail.com>
2025-10-11 20:27:24 -0400

    mini commit

--- src/main/java/Home.java
+++ src/main/java/Home.java
@@ -8,9 +8,9 @@ public class Home {
 
     public static void main(String[] args) {
 
-
         //creating the transactions arrayList
         ArrayList<Transactions> transaction = new ArrayList<>();
+
         //if save/trans app change greeting
         System.out.println("Welcome to the Accounting Ledger App");
 
@@ -28,12 +28,12 @@ public class Home {
                 L) View your accounting ledger
                 X) Exit
                 """);
-        String homeChoice = scanner.nextLine().trim();
+        String homeChoice = scanner.nextLine().trim().toUpperCase();
 
         //switch case which directs user to desired place
         switch (homeChoice) {
             case "D":
-            case "Deposit":
+            case "DEPOSIT":
                 MoneyInRecord(transaction);
                 break;
 
@@ -42,19 +42,24 @@ public class Home {
     }
 
     public static void MoneyInRecord(ArrayList<Transactions> transaction) {
-        System.out.println("How many deposits would you like to record");
-        int numOfDeposits = scanner.nextInt();
-        scanner.nextLine();
+       // int numOfDeposits;
+       // try {
+            System.out.println("How many deposits would you like to record?");
+            int numOfDeposits = scanner.nextInt();
+            scanner.nextLine();
+       // } catch ()
 
+
+        //loops through the process numOfDeposits times
         for (int i = 1; i <= numOfDeposits; i++) {
             LocalDate date = LocalDate.now();
             LocalTime time = LocalTime.now();
-            System.out.print("Enter a short description for deposit #" + i);
+            System.out.print("Enter a short description for deposit #" + i + ":" + " ");
             String description = scanner.nextLine();
-            System.out.print("Enter the name of the vendor for deposit #" + i);
+            System.out.print("Enter the name of the vendor for deposit #" + i + ":" + " ");
             String vendor = scanner.nextLine();
             //add a try & catch in case user inputs wrong type
-            System.out.print("Enter the amount deposited for deposit #" + i);
+            System.out.print("Enter the amount deposited for deposit #" + i + ":" + " ");
             double amount = scanner.nextDouble();
             scanner.nextLine();
 
