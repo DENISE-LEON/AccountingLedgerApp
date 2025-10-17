1840f0cffca508d08ce0aad3baf2b5569c117019
deniseleon <kbenisee7@gmail.com>
2025-10-16 20:13:10 -0400

    Created a loop for the home screen
    Began formatting the views using printf to align things

--- src/main/java/com/pluralsight/Home.java
+++ src/main/java/com/pluralsight/Home.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.*;
-import com.pluralsight.Ledger.*;
 
 public class Home {
     public static Scanner scanner = new Scanner(System.in);
@@ -14,26 +13,16 @@ public class Home {
 
     public static void main(String[] args) {
 
-
         //creating the transactions arrayList
-
         //if save/trans app change greeting
-        System.out.println("Welcome to the Accounting com.pluralsight.Ledger App");
 
-        String runAgain;
+        System.out.println("Welcome to the Accounting App");
 
+        //instead of running the method inside of it's self, created a loop
+        //calling method inside itself could lead to crash
+        boolean run = true;
         do {
-            appGuide();
-            System.out.println("Would you like to do something else?(Y or N)");
-            runAgain = scanner.nextLine();
-
-        } while (runAgain.equalsIgnoreCase("Y"));
-    }
-
-
-    //welcome method: asks the user what they would like to do
-    public static void appGuide() {
-        System.out.println(""" 
+            System.out.println(""" 
                 What would you like to do?
                 Your options are:
                 D) Record a deposit
@@ -41,57 +30,55 @@ public class Home {
                 L) View your accounting ledger
                 X) Exit
                 """);
-        String homeChoice = scanner.nextLine().trim().toUpperCase();
+            String homeChoice = scanner.nextLine().trim().toUpperCase();
 
-        //switch case which directs user to desired place
-        switch (homeChoice) {
-            case "D":
-            case "DEPOSIT":
-                moneyInProcess();
-                break;
-            case "W":
-            case "WITHDRAWAL":
-                moneyOutProcess();
-                break;
-            case "L":
-            case "VIEW ACCOUNTING LEDGER":
-            case "VIEW LEDGER":
-                ledgerGuide();
-                break;
-            case "X":
-            case "EXIT":
-                System.out.println("Exiting...");
-                System.exit(0);
-            default:
-                System.out.println("Invalid input. Please try again (valid input example: D)");
-                appGuide();
-
-
-                //add default for error handle
-        }
+            //switch case which directs user to desired place
+            switch (homeChoice) {
+                case "D":
+                case "DEPOSIT":
+                    moneyInProcess();
+                    break;
+                case "W":
+                case "WITHDRAWAL":
+                    moneyOutProcess();
+                    break;
+                case "L":
+                case "VIEW ACCOUNTING LEDGER":
+                case "VIEW LEDGER":
+                    Ledger.ledgerGuide(scanner, transactions);
+                    break;
+                case "X":
+                case "EXIT":
+                    System.out.println("Exiting...");
+                    run = false;
+                    break;
+                default:
+                    System.out.println("Invalid input. Please try again (valid input example: D)");
+                    //add default for error handle
+            }
+        } while (run);
     }
 
     //maybe combine money in and out
     public static void moneyInProcess() {
-        int numOfDeposits = numOfTransactions();
-
+        int numOfDeposits = numOfTransactionsPrompt();
 
         //loops through the process numOfDeposits times
         for (int i = 1; i <= numOfDeposits; i++) {
             String type = "Deposit";
             LocalDate date = LocalDate.now();
             LocalTime time = LocalTime.now();
-            System.out.print("Enter a short description for deposit #" + i + ":" + " ");
+            System.out.print("Enter a short description for deposit #" + i + ": ");
             String description = scanner.nextLine().trim();
-            System.out.print("Enter the name of the vendor for deposit #" + i + ":" + " ");
+            System.out.print("Enter the name of the vendor for deposit #" + i + ": ");
             String vendor = scanner.nextLine().trim();
-            //add a try & catch in case user inputs wrong type
 
+            //add a try & catch in case user inputs wrong type
             double amount = 0.0;
             boolean validAmtInput = false;
             do {
                 try {
-                    System.out.print("Enter the amount deposited for deposit #" + i + ":" + " ");
+                    System.out.print("Enter the amount deposited for deposit #" + i + ": ");
                     amount = Math.abs(scanner.nextDouble());
                     scanner.nextLine();
                     validAmtInput = true;
@@ -106,26 +93,25 @@ public class Home {
     }
 
     public static void moneyOutProcess() {
-        int numOfWithdrawals = numOfTransactions();
 
+        int numOfWithdrawals = numOfTransactionsPrompt();
 
         for (int i = 1; i <= numOfWithdrawals; i++) {
             String type = "Withdrawal";
             LocalDate date = LocalDate.now();
             LocalTime time = LocalTime.now();
-            System.out.print("Enter a short description for deposit #" + i + ":" + " ");
+            System.out.print("Enter a short description for withdrawal #" + i + ": ");
             String description = scanner.nextLine();
-            System.out.print("Enter the name of the vendor for deposit #" + i + ":" + " ");
+            System.out.print("Enter the name of the vendor for withdrawal #" + i + ": ");
             String vendor = scanner.nextLine();
-            //add a try & catch in case user inputs wrong type
 
+            //add a try & catch in case user inputs wrong type
             double amount = 0.0;
             boolean validAmtInput = false;
             do {
                 try {
-                    System.out.print("Enter the amount withdrawaled for withdrawal #" + i + ":" + " ");
-                    amount = Math.abs(scanner.nextDouble());
-                    amount = -Math.abs(amount);
+                    System.out.print("Enter the amount withdrawn for withdrawal #" + i + ": ");
+                    amount = -1 * Math.abs(scanner.nextDouble());
                     scanner.nextLine();
                     validAmtInput = true;
                 } catch (InputMismatchException e) {
@@ -136,17 +122,16 @@ public class Home {
             } while (!validAmtInput);
             transactionRecorder(type, date, time, description, vendor, amount);
         }
-
     }
 
-    public static int numOfTransactions() {
-        int numOfDeposits = 0;
+    public static int numOfTransactionsPrompt() {
+        int numofTransactions = 0;
         boolean validInput = false;
         //put into method
         do {
             try {
-                System.out.println("How many deposits would you like to record?");
-                numOfDeposits = Math.abs(scanner.nextInt());
+                System.out.println("How many transactions would you like to record?");
+                numofTransactions = Math.abs(scanner.nextInt());
                 scanner.nextLine();
                 validInput = true;
                 //must eat line in catch bc line eater in the try is skipped. Bad input is still in buffer
@@ -156,40 +141,34 @@ public class Home {
                 scanner.nextLine();
             }
         } while (!validInput);
-        return numOfDeposits;
+        return numofTransactions;
     }
 
     public static void transactionRecorder(String type, LocalDate date, LocalTime time, String description, String vendor, double amount) {
-        System.out.println("com.pluralsight.Transaction has been successfully recorded");
+        System.out.println();
+        System.out.println("Transaction has been successfully recorded.");
+        System.out.println();
         //the new transaction is added to the array list
         Transaction newTransaction = new Transaction(type, date, time, description, vendor, amount);
         transactions.add(newTransaction);
-        writer(newTransaction);
-        //insert writer
-        //move to transactions class?
-    }
 
-    public static String[] splitter(String line) {
-        String[] data = line.split("\\|");
-        return data;
-    }
-
-    public static void writer(Transaction newTransaction) {
+        //writer
         String filePath = "transactions.csv";
         try (PrintWriter pwriter = new PrintWriter(new BufferedWriter(new FileWriter(filePath, true)))) {
             pwriter.println(newTransaction.getType() + "|" + newTransaction.getDate() + "|" + newTransaction.getTime() + "|" + newTransaction.getDescription() + "|" + newTransaction.getVendor() + "|" + newTransaction.getAmount());
-
-
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
         } catch (IOException e) {
             System.out.println("Error");
         }
+    }
 
+    public static String[] splitter(String line) {
+        return line.split("\\|");
     }
 
     public static void reader() {
-        //change tra
+
         transactions.clear();
         String filePath = "transactions.csv";
         String line;
@@ -214,175 +193,7 @@ public class Home {
                 Comparator.comparing(Transaction::getDate)
                         .thenComparing(Transaction::getTime)
                         .reversed());
-
     }
 
-
-    public static void ledgerGuide() {
-        //calling the reader here bc most of these methods need to read from file
-        //avoids duplicate data, avoids having to clear the reader every time
-        reader();
-        System.out.println("""
-                What would you like to do?
-                Your options are:
-                A) View all transactions
-                D) View deposits
-                W) View withdrawals
-                R) View reports
-                H) Go back home
-                """);
-        String ledgerChoice = scanner.nextLine().trim().toUpperCase();
-
-        //switch case which directs user to desired place
-        switch (ledgerChoice) {
-            case "A":
-            case "VIEW ALL TRANSACTIONS":
-                Ledger.viewAllTransactions(transactions);
-                break;
-            case "D":
-            case "VIEW DEPOSITS":
-                Ledger.viewDeposits(transactions);
-                break;
-            case "W":
-            case "VIEW WITHDRAWALS":
-                Ledger.viewWithdrawals(transactions);
-                break;
-            case "R":
-            case "VIEW REPORTS":
-                Ledger.reportsGuide(scanner, transactions);
-                break;
-            case "H":
-            case "GO BACK HOME":
-                appGuide();
-                break;
-            default:
-                System.out.println("Invalid input. Please try again (valid input example: D)");
-                ledgerGuide();
-        }
-    }
 
 }
-/*
-    public static void reportsGuide() {
-
-        transaction.sort(
-                Comparator.comparing(com.pluralsight.Transaction::getDate)
-                        .thenComparing(com.pluralsight.Transaction::getTime));
-
-
-        System.out.println("""
-                How would you like to view your report?
-                Your options are:
-                M) Month to date
-                PM) Previous month
-                Y) Year to date
-                PY) Previous year
-                V) Search by vendor
-                C) Custom search
-                B) Go back to ledger page
-                """);
-        String reportChoice = scanner.nextLine().toUpperCase().trim();
-
-
-        switch (reportChoice) {
-            case "M":
-            case "MONTH TO DATE":
-                monthToDateDisplay();
-                break;
-            case "PM":
-            case "PREVIOUS MONTH":
-                previousMonthDisplay();
-                break;
-            case "Y":
-            case "YEAR TO DATE":
-                yearToDateDisplay();
-                break;
-            case "PY":
-            case "PREVIOUS YEAR":
-                previousYearDisplay();
-                break;
-            case "V":
-            case "SEARCH BY VENDOR":
-                searchByVendor();
-                break;
-
-
-        }
-
-    }
-
- *
-
-    public static void monthToDateDisplay() {
-        //setting the current day to know so reports are always up to date
-        int currentMonth = LocalDate.now().getMonthValue();
-        int currentYear = LocalDate.now().getYear();
-
-        transaction.stream()
-                .filter(t -> t.getDate().getMonthValue() == currentMonth
-                        && t.getDate().getYear() == currentYear)
-                .forEach(t -> System.out.println(
-                        t.getType() + "|" + t.getDate() + "|" + t.getTime() + "|" + t.getDescription() + "|" + t.getVendor() + "|" + t.getAmount()
-                ));
-    }
-
-    public static void previousMonthDisplay() {
-        int prevMonth = LocalDate.now().minusMonths(1).getMonthValue();
-        int currentYear = LocalDate.now().getYear();
-
-        transaction.stream()
-                .filter(t -> t.getDate().getMonthValue() == prevMonth
-                        && t.getDate().getYear() == currentYear)
-                .forEach(t -> System.out.println(
-                        t.getType() + "|" + t.getDate() + "|" + t.getTime() + "|" + t.getDescription() + "|" + t.getVendor() + "|" + t.getAmount()
-                ));
-    }
-
-    public static void yearToDateDisplay() {
-        int currentYear = LocalDate.now().getYear();
-        transaction.stream()
-                .filter(t -> t.getDate().getYear() == currentYear)
-                .forEach(t -> System.out.println(
-                        t.getType() + "|" + t.getDate() + "|" + t.getTime() + "|" + t.getDescription() + "|" + t.getVendor() + "|" + t.getAmount()
-                ));
-
-    }
-
-    public static void previousYearDisplay() {
-        int prevYear = LocalDate.now().minusYears(1).getYear();
-
-        transaction.stream()
-                .filter(t -> t.getDate().getYear() == prevYear)
-                .forEach(t -> System.out.println(
-                        t.getType() + "|" + t.getDate() + "|" + t.getTime() + "|" + t.getDescription() + "|" + t.getVendor() + "|" + t.getAmount()
-                ));
-    }
-
-    public static void searchByVendor() {
-        System.out.println("enter the name of the vendor");
-        String vendorName = scanner.nextLine();
-
-        transaction.stream()
-                .filter(t -> t.getVendor().equalsIgnoreCase(vendorName))
-                .forEach(t -> {
-                    if ()
-                        }
-                        System.out.println(
-                        t.getType() + "|" + t.getDate() + "|" + t.getTime() + "|" + t.getDescription() + "|" + t.getVendor() + "|" + t.getAmount()
-                ));
-
-    }
-}
-
-/*.filter(n -> n > 5) // keep numbers greater than 5
-        .forEach(n -> {
-        // you can still check another condition inside forEach
-        if (n % 2 == 0) {
-        System.out.println(n + " is even");
-                   } else {
-                           System.out.println(n + " is odd");
-                   }
-                           });
-
-
- */
--- src/main/java/com/pluralsight/Ledger.java
+++ src/main/java/com/pluralsight/Ledger.java
@@ -1,11 +1,102 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Scanner;
 
+import com.pluralsight.Home.*;
+
 public class Ledger {
+
+
+    public static String[] splitter(String line) {
+        //combined into one line that splits returns the split line
+        return line.split("\\|");
+    }
+
+    public static void reader(ArrayList<Transaction> transaction) {
+        //clear the arraylist to avoid duplicates
+        transaction.clear();
+        String filePath = "transactions.csv";
+        String line;
+        try (BufferedReader bReader = new BufferedReader(new FileReader(filePath))) {
+            while ((line = bReader.readLine()) != null) {
+                String[] data = splitter(line);
+
+                String type = data[0];
+                LocalDate date = LocalDate.parse(data[1]);
+                LocalTime time = LocalTime.parse(data[2]);
+                String description = data[3];
+                String vendor = data[4];
+                double amount = Double.parseDouble(data[5]);
+                transaction.add(new Transaction(type, date, time, description, vendor, amount));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+        transaction.sort(
+                Comparator.comparing(Transaction::getDate)
+                        .thenComparing(Transaction::getTime)
+                        .reversed());
+    }
+
+    public static void ledgerGuide(Scanner scanner, ArrayList<Transaction> transactions) {
+        //calling the reader here bc most of these methods need to read from file
+        //avoids duplicate data, avoids having to clear the reader every time
+        reader(transactions);
+
+        boolean run = true;
+        do {
+            System.out.println("﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌");
+            System.out.println("""
+                    What would you like to do?
+                    Your options are:
+                    A) View all transactions
+                    D) View deposits
+                    W) View withdrawals
+                    R) View reports
+                    H) Go back home
+                    """);
+            System.out.println("﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌");
+            //add pretty formatting
+            System.out.println();
+            String ledgerChoice = scanner.nextLine().trim().toUpperCase();
+
+            //switch case which directs user to desired place
+            switch (ledgerChoice) {
+                case "A":
+                case "VIEW ALL TRANSACTIONS":
+                    Ledger.viewAllTransactions(transactions);
+                    break;
+                case "D":
+                case "VIEW DEPOSITS":
+                    Ledger.viewDeposits(transactions);
+                    break;
+                case "W":
+                case "VIEW WITHDRAWALS":
+                    Ledger.viewWithdrawals(transactions);
+                    break;
+                case "R":
+                case "VIEW REPORTS":
+                    Ledger.reportsGuide(scanner, transactions);
+                    break;
+                case "H":
+                case "GO BACK HOME":
+                    run = false;
+                    break;
+                default:
+                    System.out.println("Invalid input. Please try again (valid input example: D)");
+            }
+        } while (run);
+    }
 
     public static void viewAllTransactions(ArrayList<Transaction> transaction) {
 
@@ -32,45 +123,52 @@ public class Ledger {
                 Comparator.comparing(Transaction::getDate)
                         .thenComparing(Transaction::getTime));
 
+        boolean run = true;
+        do {
+            //add in formatting
+            System.out.println();
+            System.out.println("""
+                    How would you like to view your report?
+                    Your options are:
+                    M) Month to date
+                    PM) Previous month
+                    Y) Year to date
+                    PY) Previous year
+                    V) Search by vendor
+                    C) Custom search
+                    B) Go back to ledger page
+                    """);
+            System.out.println();
 
-        System.out.println("""
-                How would you like to view your report?
-                Your options are:
-                M) Month to date
-                PM) Previous month
-                Y) Year to date
-                PY) Previous year
-                V) Search by vendor
-                C) Custom search
-                B) Go back to ledger page
-                """);
-        String reportChoice = scanner.nextLine().toUpperCase().trim();
-
-
-        switch (reportChoice) {
-            case "M":
-            case "MONTH TO DATE":
-                monthToDateDisplay(transaction);
-                break;
-            case "PM":
-            case "PREVIOUS MONTH":
-                previousMonthDisplay(transaction);
-                break;
-            case "Y":
-            case "YEAR TO DATE":
-                yearToDateDisplay(transaction);
-                break;
-            case "PY":
-            case "PREVIOUS YEAR":
-                previousYearDisplay(transaction);
-                break;
-            case "V":
-            case "SEARCH BY VENDOR":
-                searchByVendor(scanner, transaction);
-                break;
-
-
-        }
+            String reportChoice = scanner.nextLine().toUpperCase().trim();
+            switch (reportChoice) {
+                case "M":
+                case "MONTH TO DATE":
+                    monthToDateDisplay(transaction);
+                    break;
+                case "PM":
+                case "PREVIOUS MONTH":
+                    previousMonthDisplay(transaction);
+                    break;
+                case "Y":
+                case "YEAR TO DATE":
+                    yearToDateDisplay(transaction);
+                    break;
+                case "PY":
+                case "PREVIOUS YEAR":
+                    previousYearDisplay(transaction);
+                    break;
+                case "V":
+                case "SEARCH BY VENDOR":
+                    searchByVendor(scanner, transaction);
+                    break;
+                case "B":
+                case "BACK":
+                    run = false;
+                default:
+                    System.out.println("Invalid input. Please try again (valid input example: D)");
+            }
+        } while (run);
 
     }
 
@@ -86,12 +184,14 @@ public class Ledger {
     }
 
     public static void previousMonthDisplay(ArrayList<Transaction> transaction) {
-        int prevMonth = LocalDate.now().minusMonths(1).getMonthValue();
-        int currentYear = LocalDate.now().getYear();
+
+        LocalDate temp = LocalDate.now().minusMonths(1);
+        int prevMonth = temp.getMonthValue();
+        int prevYear = temp.getYear();
 
         transaction.stream()
                 .filter(t -> t.getDate().getMonthValue() == prevMonth
-                        && t.getDate().getYear() == currentYear)
+                        && t.getDate().getYear() == prevYear)
                 .forEach(t -> formatPrinter(t));
     }
 
@@ -100,19 +200,17 @@ public class Ledger {
         transaction.stream()
                 .filter(t -> t.getDate().getYear() == currentYear)
                 .forEach(t -> formatPrinter(t));
-
     }
 
     public static void previousYearDisplay(ArrayList<Transaction> transaction) {
         int prevYear = LocalDate.now().minusYears(1).getYear();
-
         transaction.stream()
                 .filter(t -> t.getDate().getYear() == prevYear)
                 .forEach(t -> formatPrinter(t));
     }
 
     public static void searchByVendor(Scanner scanner, ArrayList<Transaction> transaction) {
-        System.out.println("enter the name of the vendor");
+        System.out.println("Enter the name of the vendor: ");
         String vendorName = scanner.nextLine().trim().toLowerCase();
 
         transaction.stream()
@@ -121,12 +219,26 @@ public class Ledger {
     }
 
     public static void formatPrinter(Transaction t) {
-        System.out.println(
-                t.getType() + "|" + t.getDate() + "|" + t.getTime() + "|" + t.getDescription() + "|" + t.getVendor() + "|" + t.getAmount()
+        //formatting so everything aligns nicely
+        // %n works like new line
+        //very glad I turned this into a method
+        System.out.printf(
+                "%-12s | %-12s | %-8s | %-25s | %-15s | %10.2f%n",
+                t.getType(),
+                t.getDate(),
+                t.getTime().format(timeFormat),
+                t.getDescription(),
+                t.getVendor(),
+                t.getAmount()
         );
     }
 
+
 }
+
+
+
+
 
 
 
